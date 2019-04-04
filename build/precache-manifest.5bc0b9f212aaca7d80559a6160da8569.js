8d661a90fde00ac1b84c83e87119b1fca9122057
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2019-04-04 18:33:14 -0400

    msg

---
+++ build/precache-manifest.5bc0b9f212aaca7d80559a6160da8569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "88285bde3f6fb165d4bf",
+    "url": "/ClickyGame/static/js/main.b8831657.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/ClickyGame/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "88285bde3f6fb165d4bf",
+    "url": "/ClickyGame/static/css/main.5b215e74.chunk.css"
+  },
+  {
+    "revision": "ef9a7557c3d3bb584a0df391361f565f",
+    "url": "/ClickyGame/index.html"
+  }
+];
